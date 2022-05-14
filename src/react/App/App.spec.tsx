2852b2f426d828e929b10b6063557e43bca2089b
EnakNalla <enak.nalla@gmail.com>
2022-05-15 00:27:56 +0100

    test: :white_check_mark: add loading indicator test

--- src/react/App/App.spec.tsx
+++ src/react/App/App.spec.tsx
@@ -1,9 +1,15 @@
-import { render, screen } from '@testing-library/react';
+import { render, screen } from '../test-utils/rtl';
 import App from './App';
 
 describe('<App />', () => {
-  it('should render "Switch work Music"', () => {
+  it('should render "Loading..."', () => {
     render(<App />);
+
+    expect(screen.getByText('Loading...')).toBeInTheDocument();
+  });
+
+  it('should render "Switch work Music"', () => {
+    render(<App />, { loading: false, initData: jest.fn() });
 
     expect(screen.getByText('Switch work Music')).toBeInTheDocument();
   });
