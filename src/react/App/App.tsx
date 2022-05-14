2882f4302d74ab672a522cefbb31fe6569ed4183
EnakNalla <enak.nalla@gmail.com>
2022-05-14 23:33:27 +0100

    feat: :lipstick: add basic app layout

--- src/react/App/App.tsx
+++ src/react/App/App.tsx
@@ -1,5 +1,23 @@
+import { Image } from 'react-bootstrap';
+import styles from './App.module.scss';
+
 const App = () => {
-  return <div>App</div>;
+  return (
+    <div id={styles.gridContainer} className="text-center">
+      <div id={styles.config}>
+        <h3>Config</h3>
+      </div>
+
+      <div id={styles.main} className="text-center">
+        <h3>Switch work Music</h3>
+        <Image src="/logo.png" alt="Jellybean switch" width={64} height={64} />
+      </div>
+
+      <div id={styles.playlist} className="text-center">
+        <h3>Playlist</h3>
+      </div>
+    </div>
+  );
 };
 
 export default App;
