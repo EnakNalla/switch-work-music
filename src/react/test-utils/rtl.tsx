5835671518963fdc2ef2a8cbe614bfb8d45cc226
EnakNalla <enak.nalla@gmail.com>
2022-05-15 16:16:21 +0100

    test: :technologist: fix type for custom render store

--- src/react/test-utils/rtl.tsx
+++ src/react/test-utils/rtl.tsx
@@ -1,12 +1,18 @@
-import { render, RenderOptions, screen } from '@testing-library/react';
+import { render, RenderOptions } from '@testing-library/react';
 import { ReactNode } from 'react';
 import ProvideStore from '../stores/ProvideStore';
 import RootStore from '../stores/rootStore';
 
-const customRender = (ui: ReactNode, store?: Partial<RootStore>, opts?: RenderOptions) => {
+// https://stackoverflow.com/questions/61132262/typescript-deep-partial
+type DeepPartial<T> = T extends object
+  ? {
+      [P in keyof T]?: DeepPartial<T[P]>;
+    }
+  : T;
+
+const customRender = (ui: ReactNode, store?: DeepPartial<RootStore>, opts?: RenderOptions) => {
   return render(<ProvideStore store={store}>{ui}</ProvideStore>, opts);
 };
 
 export * from '@testing-library/react';
-export { screen };
 export { customRender as render };
